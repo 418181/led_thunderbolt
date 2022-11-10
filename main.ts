e41cda0d3c259c4e4f077c4d5ecef6f0762eabfa
418181 <115142558+418181@users.noreply.github.com>
2022-11-10 16:11:21 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
 let x = 0
-let y = 0
 let dx = 0
 basic.forever(function () {
     basic.clearScreen()
-    if (x == 4) {
+    if (x >= 4) {
         dx = -1
-    } else if (x == 0) {
+    } else if (x <= 0) {
         dx = 1
     }
     x += dx
-    led.plot(x, y)
+    led.plot(x, 0)
+    led.plot(x, 1)
+    led.plot(x, 2)
+    led.plot(x, 3)
+    led.plot(x, 4)
     basic.pause(100)
 })
